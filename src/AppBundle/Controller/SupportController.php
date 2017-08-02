391b75e3cfea7feacacc8eeef34ccee8118e0bec
Emmanuel Petit <e.petit18@laposte.net>
2017-08-02 17:12:57 +0200

    End of part 9 : added login form

--- src/AppBundle/Controller/SupportController.php
+++ src/AppBundle/Controller/SupportController.php
@@ -2,11 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\Type\ContactFormType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
 
 class SupportController extends Controller
@@ -14,18 +13,48 @@ class SupportController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        $form = $this->createFormBuilder()
-            ->add('from', EmailType::class)
-            ->add('message', TextareaType::class)
-            ->add('send', SubmitType::class)
-            ->getForm();
+        $form = $this->createForm(ContactFormType::class, null, [
+            'action' => $this->generateUrl('handle_form_submission')
+        ]);
 
-        $form->handleRequest($request);
         // replace this example code with whatever you need
         return $this->render('support/index.html.twig', [
             'our_form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @param Request $request
+     * @Route("/form-submission", name="handle_form_submission")
+     * @Method("POST")
+     */
+    public function handleFormSubmissionAction(Request $request)
+    {
+        $form = $this->createForm(ContactFormType::class);
+
+        $form->handleRequest($request);
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            return $this->redirectToRoute('homepage');
+        }
+        $data = $form->getData();
+
+        dump($data);
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Contact Form Submission')
+            ->setFrom($data['from'])
+            ->setTo('oelikqnu@sharklasers.com')
+            ->setBody(
+                $form->getData()['message'],
+                'text/plain'
+            );
+
+        $this->get('mailer')->send($message);
+
+        $this->addFlash('success', 'Your message was sent !');
+
+        return $this->redirectToRoute('homepage');
+
     }
 }
